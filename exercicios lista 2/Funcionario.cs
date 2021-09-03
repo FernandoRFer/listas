12cc983b8d0c6ed39f06097d8f597a37956b14f1
Fernando Rodrigues <88217068+FernandoRFer@users.noreply.github.com>
2021-09-03 17:10:44 -0300

    exescicios finalizados lista 2

---
+++ exercicios lista 2/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace exercicios_lista_2
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public  double Salary { get; set; }
+
+        public Funcionario(int id,string name,double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+
+        }
+
+        public void IncreaseSalary (double porcetagem)
+        {
+            Salary += Salary * porcetagem / 100;
+        }
+
+        public override string ToString()
+        {
+            return Id + "," + Name + "," + Salary.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ exercicios lista 2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace exercicios_lista_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionario> list = new List<Funcionario>();
+
+            Console.WriteLine("How many employees will be registered?");
+            int reg = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= reg; i++)
+            {
+                Console.WriteLine("Emplyoee #" + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                list.Add(new Funcionario(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase :");
+            int searched = int.Parse(Console.ReadLine());
+
+            Funcionario fun = list.Find(x => x.Id == searched);
+
+            if (fun != null)
+            {
+                Console.Write("Enter the percentage:");
+                double porce = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                fun.IncreaseSalary(porce);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+
+            foreach (Funcionario obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+
+        }
+    }
+}
